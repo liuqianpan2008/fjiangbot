d990408103ae7106a2baddf9bcf7128152afe589
枫叶秋林 <liuqianpan2018@outlook.com>
2022-08-13 10:29:54 +0800

    style: :wrench:枫叶超市功能配置

--- src/config/.config.ts
+++ src/config/.config.ts
@@ -1,5 +1,6 @@
 import { Platform } from "oicq";
 import { LogLevel } from "oicq/lib/client";
+import { type } from "os";
 
 type QQcT = {
     qq: number,
@@ -16,11 +17,18 @@ type groupT = {
     curfewTime: string | number | Date,
     curfewEndTime: string | number | Date,
     Isadmin: boolean,
+    props: Array<propT>,
 }
 type signT = {
     Issign: boolean,
     MaxGold: number,
     MinGold: number
+}
+type propT = {
+    id: number,
+    name: string,
+    price: number,
+    effect: any
 }
 let QQc: QQcT = {
     qq: 161009029,
@@ -30,28 +38,27 @@ let QQc: QQcT = {
 };
 let groupc: Map<number, groupT> = new Map([
     // 默认配置
-    [1,
-        {
-            IsgroupCod: true,//是否开启进群验证码
-            IsgroupCodTime: 10000,//单位秒
-            Iscurfew: false,//是否开启宵禁
-            //Cron表达式生成网址：https://cron.qqe2.com
-            curfewTime: "0 0 22 ? * ? ?",//宵禁开始时间 需要Cron表达式
-            curfewEndTime: "0 0 7 ? * ? ",//宵禁结束时间 需要Cron表达式
-            Isadmin: true,//是否管理员使用群管功能
-        }],
-    // 单独配置 
-    [877894787,//群号 
-        {
-            IsgroupCod: true,//是否开启进群验证码
-            IsgroupCodTime: 10000,//单位秒
-            Iscurfew: false,//是否开启宵禁
-            // 尽量不要设置同一时间段否则容易🐔
-            curfewTime: "0 0 22 ? * ? ?",//宵禁开始时间
-            curfewEndTime: "0 0 7 ? * ? ",//宵禁结束时间
-            Isadmin: true,
-        }
-    ],
+    [1, {
+        IsgroupCod: true,//是否开启进群验证码
+        IsgroupCodTime: 10000,//单位秒
+        Iscurfew: false,//是否开启宵禁
+        //Cron表达式生成网址：https://cron.qqe2.com
+        curfewTime: "0 0 22 ? * ? ?",//宵禁开始时间 需要Cron表达式
+        curfewEndTime: "0 0 7 ? * ? ",//宵禁结束时间 需要Cron表达式
+        Isadmin: true,//是否管理员使用群管功能
+        props: []//禁用道具
+    }],
+    // 单独配置
+    [877894787, {
+        IsgroupCod: true,//是否开启进群验证码
+        IsgroupCodTime: 60000,//单位毫秒
+        Iscurfew: false,//是否开启宵禁
+        // 尽量不要设置同一时间段否则容易🐔
+        curfewTime: "0 0 22 ? * ? ?",//宵禁开始时间
+        curfewEndTime: "0 0 7 ? * ? ",//宵禁结束时间
+        Isadmin: true,
+        props: []//禁用道具
+    }],
 ]);
 let signc: signT = {
     Issign: true,//是否开启签到
@@ -59,5 +66,21 @@ let signc: signT = {
     MinGold: 1//最小金币
 }
 let admins: Array<number> = [2180323481];//管理员QQ号
+let props: Array<propT> = [{
+    id: 1,//⚠️切勿随意更改⚠️
+    name: "禁言卡",
+    price: 100,
+    effect: 60//禁言时常
+}, {
+    id: 2,//⚠️切勿随意更改⚠️
+    name: "补签卡",
+    price: 100,
+    effect: ""//
+}, {
+    id: 3,//⚠️切勿随意更改⚠️
+    name: "枫叶群专属兑换卡",
+    price: 1000,
+    effect: ["123456", "123456"]
 
-export { QQc, groupc, signc, admins };
+}]
+export { QQc, groupc, signc, admins, props, };
