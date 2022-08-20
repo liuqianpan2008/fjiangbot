26aa93f6da112ce2fa0b3c7e7ff85aa5bc16c9c2
枫叶秋林 <liuqianpan2018@outlook.com>
2022-08-20 17:36:32 +0800

    upd: 优化静态界面生成速度（加入缓存

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -1,7 +1,11 @@
 import pupperteer from 'puppeteer';
 import artTemplate from "art-template";
 import fs from "fs";
+let dateMap: Map<string, string | Buffer> = new Map();
 async function HtmlImg(mode = "", data: any, id: number = 0) {
+    if (dateMap.get(mode)) {
+        return dateMap.get(mode)
+    }
     fs.mkdirSync(`${__dirname}/../resources/${mode}`, { recursive: true });
     const html = artTemplate(`${__dirname}/../../src/modular/${mode}/index.html`, data);
     await fs.writeFile(`${__dirname}/../resources/${mode}/${id === 0 ? "index" : id}.html`, html, (err: any) => {
@@ -9,13 +13,14 @@ async function HtmlImg(mode = "", data: any, id: number = 0) {
             console.log(err);
         }
     });
-    const browser = await pupperteer.launch({
-        headless: true,
-    });
+    const browser = await pupperteer.launch();
     const page = await browser.newPage();
     await page.goto(`file://${__dirname}/../resources/${mode}/${id === 0 ? "index" : id}.html`);
     let body = await page.$("#container");
     let img = await body?.screenshot({ encoding: "base64", type: "jpeg", quality: 100 }) ?? "截图失败";
+    if (id === 0) {
+        dateMap.set(mode, img);
+    }
     await browser.close();
     return img;
 }
